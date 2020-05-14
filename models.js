25903583aae2d60249abbd70341be4681e524d74
Jainam Sheth <j4inam@gmail.com>
2020-05-13 20:26:39 -0400

    Added image upload service

--- models.js
+++ models.js
@@ -5,6 +5,7 @@ exports = module.exports = (app, mongoose) => {
     require('./schema/Authentication')(app, mongoose);
     require('./schema/Location')(app, mongoose);
     require('./schema/TargetGroup')(app, mongoose);
+    require('./schema/Image')(app, mongoose);
     require('./schema/ImageCategory')(app, mongoose);
     require('./schema/Question')(app,mongoose);
     require('./schema/QuestionCategory')(app,mongoose);
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ var adminService = require("./services/admin");
 var locationService = require("./services/location");
 var targetGroupService = require("./services/targetGroups");
 var imageCategoryService = require("./services/imageCategory");
+var imageService = require("./services/image");
 var questionService = require("./services/question");
 var questionCategoryService = require("./services/questionCategory");
 var jose = require('node-jose');
@@ -103,6 +104,9 @@ exports = module.exports = (app) => {
   app.get('/api/account/imgCategory/all', imageCategoryService.getAllImageCategories);
   app.post('/api/account/imgCategory', imageCategoryService.addNewCategory);
   app.delete('/api/account/imgCategory/:imageCategoryID', imageCategoryService.deleteImageCategory);
+
+  // Image management routes
+  app.post('/api/account/image', imageService.addImage);
 
   // Question Routes 
   app.get('/api/account/questions', questionService.getAllQuestions);
